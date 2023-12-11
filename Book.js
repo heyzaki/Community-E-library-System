e5ab6606512a285d75850c1dfebb70c0333d07be
Mohammed Zaki <104645449+heyzaki@users.noreply.github.com>
2023-12-11 11:31:13 +0000

    Add files via upload

---
+++ Book.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema(
+	{
+		bookName: {
+			type: String,
+			require: true,
+		},
+		alternateTitle: {
+			type: String,
+			default: "",
+		},
+		author: {
+			type: String,
+			require: true,
+		},
+		language: {
+			type: String,
+			default: "",
+		},
+		publisher: {
+			type: String,
+			default: "",
+		},
+		bookCountAvailable: {
+			type: Number,
+			require: true,
+		},
+		bookStatus: {
+			type: String,
+			default: "Available",
+		},
+		desc: {
+			type: String,
+			default: "",
+		},
+		filename: {
+			type: String,
+			default: "",
+		},
+		categories: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: "BookCategory",
+			},
+		],
+		transactions: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: "BookTransaction",
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export default mongoose.model("Book", BookSchema);
